1205e61e7547b0b144456a5b92e635097528b350
Daniel Reyes <Dreyes007@fullsail.edu>
2013-01-06 11:38:49 -0500

    Added Bronze & Gold Files.

---
+++ Bronze/JSON.js
@@ -0,0 +1,35 @@
+var json = {
+	"information1": {
+		"fname": ["First Name:", "King"],
+		"lname": ["Last name:", "Kong"],
+		"email": ["Email:", "Kkong@lives.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"sex": ["Sex:", "Male"],
+		"age": ["Age:", "35+"],
+		"select": ["Establishments:", "Hooters"],
+		"date": ["Date of visit:", "2012-10-01"],
+		"comments": ["Comments:", "It's Alive!:"]
+	},
+	"information2": {
+		"fname": ["First Name:", "Dan"],
+		"lname": ["Last name:", "King"],
+		"email": ["Email:", "Dking@lives.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"sex": ["Sex:", "Male"],
+		"age": ["Age:", "35+"],
+		"select": ["Establishments:", "Chili's"],
+		"date": ["Date of visit:", "2012-10-01"],
+		"comments": ["Comments:", "I hope it works!:"]
+	},
+	"information3": {
+		"fname": ["First Name:", "Frank"],
+		"lname": ["Last name:", "Rizzo"],
+		"email": ["Email:", "Jerky@boys.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"sex": ["Sex:", "Male"],
+		"age": ["Age:", "35+"],
+		"select": ["Establishments:", "Applebee's"],
+		"date": ["Date of visit:", "2012-10-01"],
+		"comments": ["Comments:", "Prank?:"]
+	}
+}
