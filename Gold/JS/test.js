52637015a3ce3a696d716a8c875f468fac306926
Daniel Reyes <Dreyes007@fullsail.edu>
2013-03-01 22:52:59 -0500

    Added Theme Roller to both files and added a new branding logo

--- Gold/JS/test.js
+++ Gold/JS/test.js
@@ -25,6 +25,33 @@ $('#home').on('pageinit', function(){
 	});
 	
 	//any other code needed for addItem page goes here
+/*	    var localStorageKey = "demoStorageKey";
+        $('#personalInfo').live('pagecreate', function() {
+            if (Modernizr.localstorage) {
+                showStoreValue();
+            }
+            else {
+                $('#message').text("Unfortunately your browser doesn't support local storage");
+            //$('#submit').attr('disabled', 'disabled');
+                $('#message').show();
+            }
+            $('#submit').click(function(e) {
+                localStorage.setItem(localStorageKey, $('#fname').val());
+                showStoreValue();
+                e.preventDefault();
+            });
+        });
+
+        function showStoreValue() {
+            var item = localStorage.getItem(localStorageKey);
+            if (item == null) {
+                item = 'Nothing in store';
+            }
+            else if (item.length === 0) {
+                item = 'Store contains empty value';
+            }
+            $('.storeItem').text(item);
+        }
 	
 });
 
@@ -36,16 +63,12 @@ $('#home').on('pageinit', function(){
 
 var getData = function(){
 
-};*/
+};
+var storeData = function(data){
 
-	   var storeData = function(data){
-	   		localStorage.setItem("test","hello");
-	   		alert(localStorage.length);
 	
 }; 
 
-	var save = ge('#submit');
-	save.addEventListener("click", storeData);
 
 /*var	deleteItem = function (){
 			
